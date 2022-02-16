17fb64a7b22844954039be9b85aeac6ab6f53b02
sms <sms@ctr-it.com>
2022-02-17 07:14:54 +0900

    c22

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -1,2 +1,2 @@
 ls /jar
-gcloud compute ssh ctr-gw-was-dev-1 --zone=asia-northeast3-a --command="sudo mkdir makedirectory1"
+gcloud compute ssh ctr-gw-was-dev-1  --tunnel-through-iap --zone=asia-northeast3-a --command="sudo mkdir makedirectory1"
